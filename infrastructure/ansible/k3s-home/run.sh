a302bd89a0067553e14a95287fd323764424c1fc
Christopher Dokkeberg <christopher.dokkeberg@gmail.com>
2025-05-25 19:09:55 +0200

    added debugging

--- infrastructure/ansible/k3s-home/run.sh
+++ infrastructure/ansible/k3s-home/run.sh
@@ -5,11 +5,11 @@ ACTION=$1
 INVENTORY="inventory.ini"
 EXTRA_VARS="VMID_instance=101"
 
-ansible-galaxy collection install community.kubernetes
+ansible-galaxy collection install kubernetes.core
 
 case "$ACTION" in
   up)
-    ansible-playbook -i $INVENTORY site.yaml --extra-vars "$EXTRA_VARS"
+    ansible-playbook -vvv -i $INVENTORY site.yaml --extra-vars "$EXTRA_VARS"
     ;;
   down)
     ansible-playbook -i $INVENTORY teardown.yaml --extra-vars "$EXTRA_VARS"
